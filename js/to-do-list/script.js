0d0b7c6807e03006ab04fe711057761fefb9f4ca
Daniel Coelho Lichotti <danielclichotti@gmail.com>
2026-09-24 21:23:38 -0300

    sending codes

--- js/to-do-list/script.js
+++ js/to-do-list/script.js
@@ -1,7 +1,11 @@
+const tasksCounter = document.getElementById('tasks-counter')
+const toggleBtn = document.getElementById('toggle-btn')
 const todoList = document.getElementById('todo-list')
 const todoForm = document.getElementById('todo-form')
 const todoInput = document.getElementById('todo-input')
 const itens = document.querySelector('.item')
+let quantidade = document.querySelectorAll('ol li').length
+
 
 todoForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -12,8 +16,24 @@ todoForm.addEventListener('submit', (event) => {
     item.classList.add('item')
     item.innerText = text
     todoList.appendChild(item)
+    
+    quantidade++
+    tasksCounter.textContent = 'Quantidade: ' + quantidade
 
     item.addEventListener('click', ()=>{
         item.classList.toggle('riscado') 
     })
+
 })
+
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-theme")
+
+    if(document.body.className === 'dark-theme'){
+        toggleBtn.textContent = '☀️'
+    } else {
+        toggleBtn.textContent = '🌕'
+    }
+
+}
